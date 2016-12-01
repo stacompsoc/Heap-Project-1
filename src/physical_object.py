7ed101e005f996c7382c2c88d27cd62d96fcc6bd
Kirill Rodriguez <theoden8@gmail.com>
2016-12-01 21:43:15 +0000

    added raw implementation of the physics engine

---
+++ src/physical_object.py
@@ -0,0 +1,26 @@
+class PhysicalObject:
+    def __init__(self, mass, x, y):
+        s = self
+        s.mass, s.x, s.y = mass, x, y
+        # all tuples are to be replaced with vectors
+        s.speed, s.accel, s.forces = (0., 0.), (0., 0.), {}
+
+    def get_forces_composition():
+        force = (0., 0.)
+        for f in forces:
+            force[f][0] += f[0]
+            force[f][1] += f[1]
+        return force
+
+    def __tick__():
+        force = get_forces_composition()
+        self.x += self.speed[0]
+        self.y += self.speed[1]
+        self.speed[0] += self.accel[0]
+        self.speed[1] += self.accel[1]
+        self.accel = (f / self.mass for f in force)
+
+    def __str__(self):
+        return "point: mass=" + str(mass) \
+            + ", coordinates=(" + str(x) \
+            + ", " + str(y) + ")"
