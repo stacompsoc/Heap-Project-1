974faada3329abe903ff5461cf30b2cbacba7593
Kirill Rodriguez <theoden8@gmail.com>
2016-12-02 10:36:23 +0000

    bug fixes on physics engine

--- src/physical_object.py
+++ src/physical_object.py
@@ -3,24 +3,24 @@ class PhysicalObject:
         s = self
         s.mass, s.x, s.y = mass, x, y
         # all tuples are to be replaced with vectors
-        s.speed, s.accel, s.forces = (0., 0.), (0., 0.), {}
+        s.speed, s.accel, s.forces = [0., 0.], [0., 0.], {}
 
-    def get_forces_composition():
-        force = (0., 0.)
-        for f in forces:
+    def get_forces_composition(self):
+        force = [0., 0.]
+        for f in self.forces:
             force[f][0] += f[0]
             force[f][1] += f[1]
         return force
 
-    def __tick__():
-        force = get_forces_composition()
+    def tick(self):
+        force = self.get_forces_composition()
         self.x += self.speed[0]
         self.y += self.speed[1]
         self.speed[0] += self.accel[0]
         self.speed[1] += self.accel[1]
-        self.accel = (f / self.mass for f in force)
+        self.accel = [f / self.mass for f in force]
 
     def __str__(self):
-        return "point: mass=" + str(mass) \
-            + ", coordinates=(" + str(x) \
-            + ", " + str(y) + ")"
+        return "point: mass=" + str(self.mass) \
+            + ", coordinates=(" + str(self.x) \
+            + ", " + str(self.y) + ")"
--- src/physics_manager.py
+++ src/physics_manager.py
@@ -9,22 +9,22 @@ class PhysicsManager:
     def __init__(self):
         self.phyobjs = []
 
-    def push(phyobj):
-        self.phyobjs += phyobj
+    def push(self, p):
+        self.phyobjs.append(p)
 
     def calc_gravity_vector(self, pp):
-        force = (0., 0.)
+        force = [0., 0.]
         for p in self.phyobjs:
             if p == pp:
                 continue
-            rdiff = (p.x - pp.x, p.y - pp.y)
+            rdiff = [p.x - pp.x, p.y - pp.y]
             rdabs = sqrt(rdiff[0] ** 2 + rdiff[1] ** 2)
-            runit = (r / rdabs for r in rdiff)
+            runit = [r / rdabs for r in rdiff]
             coeff = G * p.mass * pp.mass / (rdabs ** 2)
             force[0] += -coeff * runit[0]
             force[1] += -coeff * runit[1]
 
-    def __tick__(self):
+    def tick(self):
         for p in self.phyobjs:
             p.forces['gravity'] = self.calc_gravity_vector(p)
         for p in self.phyobjs:
