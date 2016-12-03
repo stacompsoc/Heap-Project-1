2ddabdbc94146f93dceef549be4699dbe57cad86
Kirill Rodriguez <theoden8@gmail.com>
2016-12-03 15:57:25 +0000

    updated physical_object: added radius, docs

--- src/physical_object.py
+++ src/physical_object.py
@@ -1,19 +1,43 @@
+from math import pi as PI
+
+
+"""
+This entity represents a physical object, i.e. an object, which has physical matter in the context of the model.
+"""
+
+
 class PhysicalObject:
     def __init__(self, mass, x, y):
+        """
+        Initialize physical object.
+
+        :mass: mass
+        :x: x coordinate
+        :y: y coordinate
+        """
         s = self
         s.mass, s.x, s.y = mass, x, y
+        s.radius = (s.mass * (PI * 4./3)) ** (1./3)
         # all tuples are to be replaced with vectors
         s.speed, s.accel, s.forces = [0., 0.], [0., 0.], {}
 
     def get_forces_composition(self):
+        """
+        Calculate the sum of force vectors for the object.
+
+        :returns: force vector
+        """
         force = [0., 0.]
         for f in self.forces:
-            force[f][0] += f[0]
-            force[f][1] += f[1]
+            force[0] += self.forces[f][0]
+            force[1] += self.forces[f][1]
         return force
 
     def tick(self):
+        """Tick the physical object: update its physical properties."""
+        # print(str(self))
         force = self.get_forces_composition()
+        # print("force == " + str(force))
         self.x += self.speed[0]
         self.y += self.speed[1]
         self.speed[0] += self.accel[0]
@@ -21,6 +45,15 @@ class PhysicalObject:
         self.accel = [f / self.mass for f in force]
 
     def __str__(self):
+        """
+        Convert to string.
+
+        :returns: str(PhysicalObject)
+        """
         return "point: mass=" + str(self.mass) \
             + ", coordinates=(" + str(self.x) \
-            + ", " + str(self.y) + ")"
+            + ", " + str(self.y) + ")" \
+            + ", velocity = (" + str(self.speed[0]) \
+            + ", " + str(self.speed[1]) + ")" \
+            + " acceleration = (" + str(self.accel[0]) \
+            + ", " + str(self.accel[1]) + ")"
