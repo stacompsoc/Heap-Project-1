93b99e43b479b4fc4c451153f578d7cd85673612
Kirill Rodriguez <theoden8@gmail.com>
2016-12-03 15:58:50 +0000

    updated physics manager : minimal working state

--- src/physics_manager.py
+++ src/physics_manager.py
@@ -1,31 +1,48 @@
 from math import sqrt
+from scipy.constants import gravitational_constant as G
 from physical_object import *
 
 
-G = 6.67300E-11
+"""
+This entity contains and manages physical objects.
+"""
 
 
 class PhysicsManager:
     def __init__(self):
+        """Initialize physics manager."""
         self.phyobjs = []
 
     def push(self, p):
+        """
+        Add a physical object to the model.
+
+        :p: physical object
+        """
         self.phyobjs.append(p)
 
     def calc_gravity_vector(self, pp):
+        """
+        Calculate the vector of gravity force for a particular physical object.
+
+        :pp: physical object.
+
+        :returns: force vector
+        """
         force = [0., 0.]
         for p in self.phyobjs:
             if p == pp:
                 continue
-            rdiff = [p.x - pp.x, p.y - pp.y]
-            rdabs = sqrt(rdiff[0] ** 2 + rdiff[1] ** 2)
-            runit = [r / rdabs for r in rdiff]
-            coeff = G * p.mass * pp.mass / (rdabs ** 2)
-            force[0] += -coeff * runit[0]
-            force[1] += -coeff * runit[1]
+            r = [p.x - pp.x, p.y - pp.y]
+            r_len = sqrt(r[0]*r[0] + r[1]*r[1])
+            coeff = G * p.mass * pp.mass / (r_len ** 3)
+            force[0] += coeff * r[0]
+            force[1] += coeff * r[1]
+        return force
 
     def tick(self):
+        """Recomputing the forces and ticking all its elements."""
         for p in self.phyobjs:
             p.forces['gravity'] = self.calc_gravity_vector(p)
         for p in self.phyobjs:
-            tick(p)
+            p.tick()
