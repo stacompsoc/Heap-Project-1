90309e3f638692a45a8461a654dabe94add82c39
PolarBearSaviour <kareem2910@googlemail.com>
2016-12-10 18:10:09 +0000

    Rewrote vector to be iterable and remove unneeded functionality

--- src/vector.py
+++ src/vector.py
@@ -2,106 +2,133 @@ import math
 import copy
 
 class Vector:
+    """
+        Parameters: Takes in a list of numeric components
+    """
     def __init__(self, components):
-        s = self;
-        s._components = components
+        self._components = components
+        self._index = 0
+        self._max = len(components)
 
-    def __str__(s):
-        string = ""
-        for index in range(len(s._components)):
-            string += str(s._components[index])
-            string += " "
-
+    # Returns string representation to print
+    def __str__(self):
+        string = "("
+        for i, part in enumerate(self):
+            if i != self._max - 1:
+                string += str(part) + ", "
+            else:
+                string += str(part)
+        string += ")"
         return string
 
-    def getComponents(s):
-        return s._components
+    # Returns the length of the object
+    def __len__(self):
+        return (self._max - 1)
+
+    # Makes the object an iterable (like a list or string)
+    def __iter__(self):
+        return self
+
+    # Allows the object to be indexed into and changed
+    def __setitem__(self, index, value):
+        self._components[index] = value
+
+    # Allows user to get a value at a certain index in the object
+    def __getitem__(self, index):
+        return self._components[index]
+
+    # Allows the object to be iterated over
+    def __next__(self):
+        if self._index == self._max:
+            self._index = 0
+            raise StopIteration
+        else:
+            value = self._components[self._index]
+            self._index += 1
+            return value
 
     """
-        Adds a vector to the current vector
-        object
+        Checks whether the vectors have the same number of components.
+        For internal use only
     """
-    def addVector(s, vector):
-        compSecondVector = vector.getComponents()
-        for index in range(len(compSecondVector)):
-            s._components[index] += compSecondVector[index]
+    def _checkMistmatchingVector(self, vector):
+        if self._max - 1 != len(vector):
+            raise Exception("Mismatching number of components")
 
     """
-        Multiples the current vector by a
-        given constant
+        Adds the vector passed to the current object (vector)
     """
-    def mutlipleByConstant(s, constant):
-        for index in range(len(s._components)):
-            s._components[index] *= constant
+    def add(self, vector):
+        self._checkMistmatchingVector(vector)
+        for i, element in enumerate(vector):
+            self[i] += element
 
     """
-        substracts antoher vector from the current
-        vector object
+        Substracts the vector passed from the current object (vector)
     """
-    def substractVector(s, secondVector):
-        secondVector.multipleByScalar(-1)
-        s.addVector(secondVector)
+    def substract(self, vector):
+        self._checkMistmatchingVector(vector)
+        for i, element in enumerate(vector):
+            self[i] -= element
 
     """
-        Divides the current vector by a given scalar
+        Mutliples the current object (vector) by the vector passed
     """
-    def divideByScalar(s, scalar):
-        scalar = 1/scalar
-        s.multipleByScalar(scalar)
+    def mutliple(self, vector):
+        self._checkMistmatchingVector(vector)
+        for i, component in enumerate(vector):
+            self[i] *= component
 
     """
-        Multiples the current vector object by another
-        vector
+        Mutliples the current object (vector) by a constant
     """
-    def multipleVectors(s, vector, returnNewVector = False):
-        numOfIteration = len(vector) if len(vector) < len(s._components) else len(s._components)
-        secondVectorComponents = vector.getComponents()
-        for index in range(numOfIteration):
-            s._components[index] *= secondVectorComponents[index]
+    def mutlipleByConstant(self, constant):
+        for i in range(self._max):
+            self[i] *= 5
 
     """
-        Works out the dot product of the current vector and another
-        vector.
-
-        Return: the dot product
+        Divides the current object (vector) by a constant
     """
-    def workOutDotProduct(s, secondVector):
+    def divideByConstant(self, constant):
+        constant = 1/constant
+        self.multipleByScalar(constant)
+
+    """
+        Works out the dot product for the current vector
+        and the vector passed
+
+        Returns: the dot product
+    """
+    def dotProduct(self, vector):
         dotProduct = 0
-        for element1, element2 in zip(s._components, secondVector):
-            dotProduct += element1 * element2
+        self._checkMistmatchingVector(vector)
+
+        for index, element1 in enumerate(self):
+            dotProduct += element1 * vector[index]
         return dotProduct
 
+    """
+        Calculates the magnitude of the current vector
 
+        Returns: the magnitude
+    """
+    def magnitude(self):
+      magnitude = 0
+      for currentElement in self:
+          magnitude += currentElement**2
 
+      magnitude = math.sqrt(magnitude)
+      return magnitude
 
     """
-        Works out the magnitude of the current vector object
+        Calculates the angle between two vector
 
-        Return: the magnitude of the vector
+        Returns: the angle in radians
     """
-    def workOutMagnitude(s):
-        magnitude = 0
-        for currentElement in s._components:
-            magnitude += currentElement^2
-
-        magnitude = math.sqrt(magnitude)
-
-        return magnitude
-
-
-    def workOutVectorProduct(s, secondVector):
-        if(len(s._components) == len(secondVector)):
-            pass
-
-def main():
-    v1 = Vector([1,2,3,4])
-    print(v1)
-    v1.mutlipleByConstant(5)
-    print(v1)
-
-    v2 = Vector([5,6,7,8])
-    v1.addVector(v2)
-    print(v1)
-
-if __name__ == '__main__':
-    main()
+    def angleBetween(self, vector):
+        mag1 = self.magnitude()
+        print(mag1)
+        mag2 = vector.magnitude()
+        print(mag2)
+        dotProduct = self.dotProduct(vector)
+        return math.acos(dotProduct/(mag1*mag2))
