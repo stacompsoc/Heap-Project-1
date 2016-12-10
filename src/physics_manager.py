5f89fe59b3dacdd7576400c3dbbb1ed39f19281f
PolarBearSaviour <kareem2910@googlemail.com>
2016-12-10 19:17:42 +0000

    Started working on the physical manager. Not sure what partial radius is doing though

--- src/physics_manager.py
+++ src/physics_manager.py
@@ -1,9 +1,8 @@
 from math import sqrt
-
+from copy import deepcopy
 from constants import *
 from physical_object import *
-
-#sajfjksdahfkjahskfhsadkhfsak
+from vector import Vector
 """
 This entity contains and manages physical objects.
 """
@@ -30,31 +29,45 @@ class PhysicsManager:
 
         :returns: force vector
         """
-        force = [0., 0.]
+        force = Vector([0,0])
 
         def _volume(r):
             return 4./3*PI*(r**3)
 
         def _partial_radius(p1, p2):
+            # Works out the distance between points
             range = sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2) * SCALE_FACTOR
+
+            #Checks whether the objects are touching one another surface or not
             if range >= p1.radius + p2.radius:
-                return [1., 1.]
+                return Vector([1., 1.])
+
+
             d, r1, r2 = range, p1.radius, p2.radius
+
+            # Gets the distance between the two objects
             r = r1 + r2 - d
+
             v = r*r*(PI/(12.*d))*(d*d + 2*d*(r1 + r2) - 3*((r1 - r2)**2))
+
             first = 0. if range <= p1.radius else 1. - v/_volume(r1)
             second = 0. if range <= p2.radius else 1. - v/_volume(r2)
+
             return [first, second]
 
         for p in self.phyobjs:
+            # Skips over itself
             if p == pp:
                 continue
-            r = [p.x - pp.x, p.y - pp.y]
-            r_len = sqrt(r[0]*r[0] + r[1]*r[1])
+
+            # Copies the position vector and then works out the distance between the two points
+            pos = copy.deepcopy(p.position)
+            pos.substract(pp.position)
+            distance = pos.magnitude()
+
             decr = _partial_radius(p, pp)
-            coeff = G * decr[0]*p.mass * decr[1]*pp.mass / (r_len ** 3)
-            force[0] += coeff * r[0]
-            force[1] += coeff * r[1]
+            coeff = G * decr[0]*p.mass * decr[1]*pp.mass / (distance ** 3)
+            force.add([coeff * r[0], coeff * r[1]])
         return force
 
     def tick(self):
