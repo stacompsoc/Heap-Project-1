324e0b18505942f7896d57c5f29edad7d0b88a53
Malik Ahmad <Malik Ahmad>
2016-12-19 19:49:44 +0000

    Code now runs without scripy (currently working on system without it)

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,15 @@
-from math import sqrt, pi
+from math import sqrt
+
+try:
+    from scipy.constants import *
+    G = gravitational_constant * 1e7
+    LY = light_year
+except ImportError:
+    G = 6.67408e-11 * 1e7
+    LY = 9.4607e15
 
 SCALE_FACTOR = 1e8
 COLLISION_TRANSFER = 1./4
 MIN_MASS = SCALE_FACTOR**3
 
-G = 6.67408e-11 * 1e7
 PI = pi
