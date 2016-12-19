3b584adce3a531019c15e6d8ea09401718d7b49d
Malik Ahmad <Malik Ahmad>
2016-12-19 18:09:38 +0000

    Added some docstrings...

--- src/vector.py
+++ src/vector.py
@@ -40,6 +40,13 @@ class Vector:
 
 
     def __add__(self, v2):
+        """
+            Method allows vectors to be added
+            like normal numeric value (i.e. a + b).
+            Well throw an excption if the vectors don't
+            have the same dimensions.
+            Returns: the resultant vector of the operation
+        """
         self._checkDimension(v2)
         v3 = self._createEmptyVector(self._max)
         for element in range(self._max):
@@ -47,13 +54,26 @@ class Vector:
         return v3
 
     def __sub__(self, v2):
+        """
+            Methods allows vector to be substracted
+            like normal numeric values (i.e. a - b).
+            Again throw an exception if dimensions and
+            will return resultant vector
+        """
         self._checkDimension(v2)
         v3 = self._createEmptyVector(self._max)
         for element in range(self._max):
             v3[element] = self[element] - v2[element]
         return v3
 
+
     def __mul__(self, v2):
+        """
+            Allows the vector to be multiplied like
+            a normal numeric value (a*b). Will return
+            the dot-product of two vectors or the vector
+            multiplied by the constant
+        """
         if(isinstance(v2, Vector)):
             self._checkDimension(v2)
             dotProduct = 0
@@ -67,25 +87,22 @@ class Vector:
             return v3
 
     def __truediv__(self, num):
+        """
+            Allows vectors to be divided like normal numeric
+            values (though not integer division) (a/b).
+        """
         v2 = self._createEmptyVector(self._max)
         for index in range(self._max):
             v2[index] = float(self[index]/num)
         return v2
 
     def calMagnitude(self):
+        """
+            Works out the magnitude of the
+            current vector and returns it
+        """
         magnitude = 0.0
         for element in self:
             magnitude += element**2
         magnitude = sqrt(magnitude)
         return magnitude
-
-def main():
-    v1 = Vector(1,2,3)
-    v2 = v1*4
-    print(v2)
-    v2 = v1/4
-    print(v2)
-    mag = v2.calMagnitude()
-    print(mag)
-if __name__ == '__main__':
-    main()
