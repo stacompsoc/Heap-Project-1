2509310520671a1283016d26453b0b6a53013d9d
Kirill Rodriguez <theoden8@gmail.com>
2017-01-08 17:06:06 +0200

    removed scipy

--- src/constants.py
+++ src/constants.py
@@ -1,13 +1,9 @@
-from math import sqrt
+from math import *
 
-try:
-    from scipy.constants import *
-    G = gravitational_constant * 1e7
-    LY = light_year
-except ImportError:
-    from math import pi
-    G = 6.67408e-11 * 1e7
-    LY = 9.4607e15
+
+G = 6.67408e-11 * 1e7
+LY = 9.4607e15
+
 
 SCALE_FACTOR = 1e8
 COLLISION_TRANSFER = 1./4
